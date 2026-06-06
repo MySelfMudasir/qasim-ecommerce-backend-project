1873cd999e2c8fb9e11ed1231ab89acf0e007413
Mudasir Maqbool <mudasir.maqbool@ablfunds.com>
2026-06-06 23:00:39 +0500

    live

--- config/db.js
+++ config/db.js
@@ -5,13 +5,13 @@ dotenv.config();
 const { Pool } = pg;
 
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
 
 const pool = new Pool({
